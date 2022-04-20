878a3789572b0d9e27aacc3663c6b5db30da2c01
tse-egor <82537848+tse-egor@users.noreply.github.com>
2022-04-20 13:11:23 +0300

    done auth and cards db-table models

--- app/model/all_models/__all_models.py
+++ app/model/all_models/__all_models.py
@@ -1,1 +1,2 @@
 from . import auth_models
+from . import cards_models
--- app/model/all_models/cards_models.py
+++ app/model/all_models/cards_models.py
@@ -1,1 +1,46 @@
+from enum import unique
 import sqlalchemy
+from ..settings.session_db import SqlAlchemyBase
+
+
+class Cards(SqlAlchemyBase):
+    __tablename__ = 'cards'
+
+    id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    card_name = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True
+    )
+    cashback = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        nullable=True
+    )
+    annual_fee = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        nullable=True
+    )
+    credit_score = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        nullable=True
+    )
+    network = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True
+    )
+    regular_apr = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True
+    )
+    bank_name = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True
+    )
+    img_path = sqlalchemy.Column(
+        sqlalchemy.String,
+        unique=True,
+        nullable=True
+    )
