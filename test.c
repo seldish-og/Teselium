1c804eeeebaacdf68b2a48a1426b6aef0bb17e73
George Tselischev <jegorbasya@mail.ru>
2023-03-02 19:34:28 +0300

    Update test.c

--- test.c
+++ test.c
@@ -866,3 +866,24 @@ int main()
     printDiamond(8);
     return 0;
 }
+
+// C program to Demonstrate Floyd's Triangle
+// Using for loop
+#include <stdio.h>
+void floyd(int n)
+{
+    int i, j = 1;
+ 
+    // Condition printing the number of element
+    for (i = 1; i <= (n * (n + 1)) / 2; i++) {
+ 
+        printf("%d ", i);
+ 
+        // condition for row of number of element printing
+        if (i == (j * (j + 1)) / 2) {
+            printf("\n");
+            j++;
+        }
+    }
+}
+int main() { floyd(6); }
