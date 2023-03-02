36139d7e493007ccb8f3727e9b9f4bff327a6481
George Tselischev <jegorbasya@mail.ru>
2023-03-02 19:13:12 +0300

    Update test.c

--- test.c
+++ test.c
@@ -70,3 +70,25 @@ int main()
 
     return 0;
 }
+
+// GCD Using while loop and if...else Statement
+
+#include <stdio.h>
+int main()
+{
+    int n1, n2;
+    
+    printf("Enter two positive integers: ");
+    scanf("%d %d",&n1,&n2);
+
+    while(n1!=n2)
+    {
+        if(n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+    printf("GCD = %d",n1);
+
+    return 0;
+}
