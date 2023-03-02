03435711693eee4399649d334bbb18b3d7faf599
George Tselischev <jegorbasya@mail.ru>
2023-03-02 19:19:08 +0300

    Update test.c

--- test.c
+++ test.c
@@ -255,3 +255,18 @@ int main() {
     fclose(fptr);
     return 0;
 }
+
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
