637deacee13ba20bc0108f6483f7f255a1cf283c
George Tselischev <jegorbasya@mail.ru>
2023-03-02 19:21:25 +0300

    Update test.c

--- test.c
+++ test.c
@@ -318,3 +318,27 @@ int main() {
    }
    return 0;
 }
+
+// Inverted half pyramid of *
+
+// * * * * *
+// * * * *
+// * * * 
+// * *
+// *
+
+// C Program
+
+#include <stdio.h>
+int main() {
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = rows; i >= 1; --i) {
+      for (j = 1; j <= i; ++j) {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
