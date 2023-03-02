217fc891e9de6160747f97cdab6fcacc8f3fb1e1
George Tselischev <jegorbasya@mail.ru>
2023-03-02 19:27:02 +0300

    Update test.c

--- test.c
+++ test.c
@@ -491,3 +491,24 @@ int main() {
    }
    return 0;
 }
+// Floyd's Triangle.
+
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
+
+#include <stdio.h>
+int main() {
+   int rows, i, j, number = 1;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; i++) {
+      for (j = 1; j <= i; ++j) {
+         printf("%d ", number);
+         ++number;
+      }
+      printf("\n");
+   }
+   return 0;
+}
