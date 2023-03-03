8a7bc4f0a5c6c4f2c86d21892f1b2d4234d1ee18
George Tselischev <jegorbasya@mail.ru>
2023-03-03 10:39:23 +0300

    Update test2.c

--- test2.c
+++ test2.c
@@ -737,3 +737,43 @@ int main()
    
     return 0;
 }
+// C program to concatenating two
+// strings using function
+#include <stdio.h>
+#include <string.h>
+ 
+void concatenate_string(char* s, char* s1)
+{
+    int i;
+ 
+    int j = strlen(s);
+ 
+    for (i = 0; s1[i] != '\0'; i++) {
+        s[i + j] = s1[i];
+    }
+ 
+    s[i + j] = '\0';
+ 
+    return;
+}
+ 
+int main()
+{
+ 
+    char s[5000], s1[5000];
+ 
+    printf("Enter the first  string: ");
+    gets(s);
+ 
+    printf("Enter the second string: ");
+    gets(s1);
+ 
+    // function concatenate_string
+    // called and s and s1 are
+    // passed
+    concatenate_string(s, s1);
+ 
+    printf("Concatenated String is: '%s'\n", s);
+ 
+    return 0;
+}
