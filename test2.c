68a587fc195a66953e51b296ae66452beccdb845
George Tselischev <jegorbasya@mail.ru>
2023-03-03 10:16:49 +0300

    Update test2.c

--- test2.c
+++ test2.c
@@ -590,3 +590,62 @@ int main()
         printf("not a square matrix\n");
     return 0;
 }
+	       
+	       // C program interchange the elements
+// of first and last in a matrix
+// across rows.
+#include <stdio.h>
+ 
+#define n 3
+ 
+// Function to swap the element
+// of first and last row
+void interchangeFirstLast(int m[][n])
+{
+    int rows = n;
+ 
+    // Swapping of element between first
+    // and last rows
+    for (int i = 0; i < n; i++)
+    {
+        int t = m[0][i];
+        m[0][i] = m[rows - 1][i];
+        m[rows - 1][i] = t;
+    }
+}
+ 
+// Driver code
+int main()
+{
+    // Input matrix
+    int m[n][n] = {{6, 3, 1},
+                   {4, 5, 2},
+                   {2, 4, 9}};
+ 
+    // Printing the input matrix
+    printf("Input Matrix: \n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+ 
+    // call interchangeFirstLast(m) function.
+    // This function swap the element of
+    // first and last row
+    interchangeFirstLast(m);
+ 
+    // Printing the original matrix
+    printf("\nOutput Matrix: \n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
