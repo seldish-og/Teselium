5b90d2dceeffe5a09e712ceb91f0ad91d824da9b
George Tselischev <jegorbasya@mail.ru>
2023-03-04 19:18:46 +0300

    Update test3.c

--- test3.c
+++ test3.c
@@ -469,3 +469,19 @@ int main()
     printf("\n\n\t\t\tCoding is Fun !\n\n\n");
     return 0;
 }
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+    int rad;
+    float PI = 3.14, area, ci;
+    printf("\nEnter the radius of the circle: ");
+    scanf("%d", &rad);
+    area = PI*rad*rad;
+    printf("\n\n\n Area of the circle is: %f ", area);
+    ci = 2*PI*rad;
+    printf("\n\n\n Circumference of the circle is: %f", ci);
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
