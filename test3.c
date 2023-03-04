7847c91510f9efd951e2c27061a0e588173b0db2
George Tselischev <jegorbasya@mail.ru>
2023-03-04 19:16:38 +0300

    Update test3.c

--- test3.c
+++ test3.c
@@ -403,3 +403,54 @@ int main()
     printf("\n\n\t\t\tCoding is Fun !\n\n\n");
     return 0;
 }
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+
+    double a, b, c;
+
+    printf("Enter 3 numbers:\n");
+    scanf("%lf%lf%lf", &a, &b, &c);  //lf is a format specifier to take double as input
+
+    // a is the largest
+    if(a >= b && a >= c)
+    {
+        if(b >= c)
+        {
+            /*
+                .2lf restricts the number till 
+                2 decimal places
+            */
+            printf("\n\n%.2lf is the 2nd largest number\n", b);
+        }
+        else
+        {
+            printf("\n\n%.2lf is the 2nd largest number\n", c);
+        }
+    }
+    else if(b >= a && b >= c)
+    {
+        if(a >= c)
+        {
+            printf("\n\n%.2lf is the 2nd largest number\n",a);
+        }
+        else
+        {
+            printf("\n\n%.2lf is the 2nd largest number\n",c);
+        }
+    }
+
+    // c is the largest number of the three
+    else if(a >= b)
+    {
+        printf("\n\n%.2lf is the 2nd largest number\n", a);
+    else
+    {
+        printf("\n\n%.2lf is the 2nd largest number\n", b);
+    }
+
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
