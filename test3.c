b53120de883da9dd27abc44ba27a64bd554f27b5
George Tselischev <jegorbasya@mail.ru>
2023-03-04 19:22:12 +0300

    Update test3.c

--- test3.c
+++ test3.c
@@ -533,3 +533,26 @@ int main()
     printf("\n\n\t\t\tCoding is Fun !\n\n\n");
     return 0;
 }
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+    int a, b, add, subtract, multiply;
+    float divide;
+
+    printf("Enter two integers: \n");
+    scanf("%d%d", &a, &b);
+
+    add = a+b;
+    subtract = a-b;
+    multiply = a*b;
+    divide = a/b;
+
+    printf("\nAddition of the numbers = %d\n", add);
+    printf("Subtraction of 2nd number from 1st = %d\n", subtract);
+    printf("Multiplication of the numbers = %d\n", multiply);
+    printf("Dividing 1st number from 2nd = %f\n", divide);
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
