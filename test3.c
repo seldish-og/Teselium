fe94075d9ded9780dc8c7e624e76a7c2670a8d76
George Tselischev <jegorbasya@mail.ru>
2023-03-04 19:04:14 +0300

    Update test3.c

--- test3.c
+++ test3.c
@@ -84,3 +84,20 @@ int main() {
   fclose(fptr);
   return 0;
 }
+#include <stdio.h>
+
+enum Level {
+  LOW,
+  MEDIUM,
+  HIGH
+};
+  
+int main() {
+  // Create an enum variable and assign a value to it
+  enum Level myVar = MEDIUM;
+ 
+  // Print the enum variable
+  printf("%d", myVar);
+  
+  return 0;
+}
