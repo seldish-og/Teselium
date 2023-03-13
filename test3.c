d1ab6a01b46b1927acbfac8e4991aff8a4b2d9a6
George Tselischev <jegorbasya@mail.ru>
2023-03-13 12:03:38 +0300

    Update test3.c

--- test3.c
+++ test3.c
@@ -730,3 +730,28 @@ int main()
     getch();
     return 0;
 }
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+    char str1[50], str2[50];
+    int len1, len2;
+    printf("Enter First String: ");
+    gets(str1);
+    printf("Enter Second String: ");
+    gets(str2);
+    len1 = strlen(str1);
+    len2 = strlen(str2);
+    if(len1==len2)
+    {
+        if(strcmp(str1, str2)==0)
+            printf("\nStrings are Equal");
+        else
+            printf("\nStrings are not Equal");
+    }
+    else
+        printf("\nStrings are not Equal");
+    getch();
+    return 0;
+}
